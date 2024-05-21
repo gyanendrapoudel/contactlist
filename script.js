abfcd0b712aa7f35bb634c18a7c9258a5190ccb3
Gyanendra <gyanen56@gmail.com>
2024-05-21 10:23:16 +1000

    displaying accordin using JS

--- script.js
+++ script.js
@@ -28,5 +28,42 @@ const fetchUser = async (url)=>{
    const result = await response.json()
    userList=result.results
    console.log(userList)
+
+//  hide spinner
+    const spin = document.querySelector('.showSpinner')
+    spin.remove()
+
+
+    // show contact user
+    displayContactList(userList)
+
+
 }
 fetchUser(apiEP)
+
+// display user list function
+const displayContactList = (userList)=>{
+    
+    document.querySelector('#list').style.display="block"
+
+    const accordion = document.querySelector('.accordion')
+    let accordionHTML=''
+    let numbers = ["One", "Two", "Three", "Four"]
+    userList.map((user,i)=>{
+        accordionHTML+=`<div class="accordion-item">
+                                    <h2 class="accordion-header">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${numbers[i]}" aria-expanded="false" aria-controls="collapse${numbers[i]}">
+                                        Accordion Item #3
+                                    </button>
+                                    </h2>
+                                    <div id="collapse${numbers[i]}" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                    </div>
+                                    </div>
+                            </div>`
+    })
+    accordion.innerHTML = accordionHTML
+    console.log(accordionHTML)
+
+}
